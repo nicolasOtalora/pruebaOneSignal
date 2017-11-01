812f1ee4a0a94a05644dcfec3f8b81defb2ff689
Nicolas <Nicolas@192.168.0.7>
2017-10-31 21:12:10 -0500

    Merge nico into Development
    Conflicts:
    	AppUAlfa/web/listarTiendas.jsp
    Crear tienda, agregar productos y manejo de sesión.

--- src/main/java/com/crunchify/jsp/servlet/HelloCrunchify.java
+++ src/main/java/com/crunchify/jsp/servlet/HelloCrunchify.java
@@ -1,33 +1,35 @@
 package com.crunchify.jsp.servlet;
- 
+
 import java.io.IOException;
+import java.io.PrintWriter;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.util.List;
 import javax.servlet.RequestDispatcher;
- 
+
 /**
  * @author Crunchify.com
  */
- 
 public class HelloCrunchify extends HttpServlet {
+
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         // reading the user input
-        String correo = request.getParameter("correo");
+
         
-        //Se debe incluir validaciones - Lo recuerda: Gestion de Excepciones.
+            String correo = request.getParameter("correo");
+            System.out.println("holaalalalallala");
+
+            //Se debe incluir validaciones - Lo recuerda: Gestion de Excepciones.
+            //Listando la informacion  
+            SendMail sender = new SendMail();
+            sender.sendMail(correo);
+
+            //Redireccionando la informacion
+            RequestDispatcher redireccion = request.getRequestDispatcher("index.jsp");
+            redireccion.forward(request, response);
         
-        //Listando la informacion  
-        SendMail sender = new SendMail();
-        sender.sendMail(correo);
-      
-       
-        //Redireccionando la informacion
-        RequestDispatcher redireccion = request.getRequestDispatcher("index.jsp");
-        redireccion.forward(request, response);
-        
-        
-        }
+
+    }
 }
