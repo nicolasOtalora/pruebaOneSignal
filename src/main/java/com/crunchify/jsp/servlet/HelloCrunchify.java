3a8bacbd2487b8a3b14d637c7cea7c255827e539
Nicolas <Nicolas@192.168.0.11>
2017-11-09 23:23:43 -0500

    Prueba con Sendgrid

--- src/main/java/com/crunchify/jsp/servlet/HelloCrunchify.java
+++ src/main/java/com/crunchify/jsp/servlet/HelloCrunchify.java
@@ -29,7 +29,7 @@ public class HelloCrunchify extends HttpServlet {
             //Listando la informacion  
 //            SendMail sender = new SendMail();
         try {
-            SendMail.send("nicolas.otalora@correo.usa.edu.co",correo,"Prueba de correo","<h1>Sirvió el correoooo hptaaaas</h1>");
+            SendMail.send("appumartsw@gmail.com",correo,"Prueba de correo","<h1>Sirvió el correoooo hptaaaas</h1>");
         } catch (Exception ex) {
             Logger.getLogger(HelloCrunchify.class.getName()).log(Level.SEVERE, null, ex);
         }
